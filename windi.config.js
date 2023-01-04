8536a3ac708ef4cb8b081619dd5e2c56c9fdc44d
Mufanc <mufanc.xyz@gmail.com>
2023-01-04 22:21:10 +0800

    refactor: adjust style

--- windi.config.js
+++ windi.config.js
@@ -1,15 +1,44 @@
 module.exports = {
-	extract: {
-		include: [
-			'content/**/*.md', 
-			'themes/overlay/layouts/**/*.html'
-		],
-		exclude: [
+    extract: {
+        include: [
+            'content/**/*.md', 
+            'themes/overlay/layouts/**/*.html'
+        ],
+        exclude: [
             '.git/**/*',
-			'node_modules/**/*'
-		]
-	},
-	preflight: true,
+            'node_modules/**/*'
+        ]
+    },
+    theme: {
+        extend: {
+          typography: {
+                DEFAULT: {
+                    css: {
+                        '*:not(pre > code *)': {
+                            'color': 'var(--content)'
+                        },
+                        ':is(h1, h2, h3, h4, h5, h6)': {
+                            'color': 'var(--primary)'
+                        },
+                        'a': {
+                            'text-decoration': 'none',
+                        },
+                        'code': {
+                            '&::before, &::after': {
+                                'display': 'none'
+                            }
+                        },
+                        ':is(strong, code)': {
+                            'color': 'inherit'
+                        },
+                        'pre': {
+                            'background-color': 'inherit !important'
+                        }
+                    },
+                },
+            },
+        },
+    },
     plugins: [
         require('windicss/plugin/typography')
     ]
